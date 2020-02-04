a55ff3842a6393d1ab1bb349f0d8be14123fe5a5
Alex <dnjscksdn98@gmail.com>
2020-02-04 23:04:06 +0900

    Wire up the checkout view

--- core/urls.py
+++ core/urls.py
@@ -2,22 +2,22 @@ from django.urls import path
 from .views import (
     HomeView,
     ItemDetailView,
-    checkout,
     add_to_cart,
     remove_from_cart,
     OrderSummaryView,
-    remove_single_item_from_cart
+    remove_single_item_from_cart,
+    CheckoutView
 )
 
 app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('checkout/', checkout, name='checkout'),
-    path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
     path('remove-single-item-from-cart/<slug>/',
-         remove_single_item_from_cart, name='remove-single-item-from-cart')
+         remove_single_item_from_cart, name='remove-single-item-from-cart'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
 ]
