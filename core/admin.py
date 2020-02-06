75bde41415e9934d68c1b112d2abf47915135ab2
Alex <dnjscksdn98@gmail.com>
2020-02-06 19:11:00 +0900

    Create address custom admin site

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Item, OrderItem, Order, Payment, Coupon, Refund
+from .models import Item, OrderItem, Order, Payment, Coupon, Refund, Address
 
 
 # create custom action
@@ -19,12 +19,14 @@ class OrderAdmin(admin.ModelAdmin):
         'refund_requested',
         'refund_granted',
         'billing_address',
+        'shipping_address',
         'payment',
         'coupon'
     ]
     list_display_links = [
         'user',
         'billing_address',
+        'shipping_address',
         'payment',
         'coupon'
     ]
@@ -43,9 +45,33 @@ class OrderAdmin(admin.ModelAdmin):
     ]
 
 
+class AddressAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'street_address',
+        'apartment_address',
+        'country',
+        'zip',
+        'address_type',
+        'default'
+    ]
+    list_filter = [
+        'default',
+        'address_type',
+        'country'
+    ]
+    search_fields = [
+        'user',
+        'street_address',
+        'apartment_address',
+        'zip'
+    ]
+
+
 admin.site.register(Item)
 admin.site.register(OrderItem)
 admin.site.register(Order, OrderAdmin)
 admin.site.register(Payment)
 admin.site.register(Coupon)
 admin.site.register(Refund)
+admin.site.register(Address, AddressAdmin)
