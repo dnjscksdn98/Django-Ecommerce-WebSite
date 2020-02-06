d96f1c46121d6ef4912f918f5346c2833df70b4b
Alex <dnjscksdn98@gmail.com>
2020-02-06 19:10:40 +0900

    Modify address model

--- core/models.py
+++ core/models.py
@@ -16,6 +16,11 @@ LABEL_CHOICES = (
     ('D', 'danger')
 )
 
+ADDRESS_CHOICES = (
+    ('B', 'Billing'),
+    ('S', 'Shipping')
+)
+
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
@@ -79,8 +84,13 @@ class Order(models.Model):
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
     ordered = models.BooleanField(default=False)
+
+    # add a 'related_name' because it has the same related model
+    shipping_address = models.ForeignKey(
+        'Address', on_delete=models.SET_NULL, related_name='shipping_address', blank=True, null=True)
     billing_address = models.ForeignKey(
-        'BillingAddress', on_delete=models.SET_NULL, blank=True, null=True)
+        'Address', on_delete=models.SET_NULL, related_name='billing_address', blank=True, null=True)
+
     payment = models.ForeignKey(
         'Payment', on_delete=models.SET_NULL, blank=True, null=True)
     coupon = models.ForeignKey(
@@ -109,16 +119,21 @@ class Order(models.Model):
         return total
 
 
-class BillingAddress(models.Model):
+class Address(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
     street_address = models.CharField(max_length=100)
     apartment_address = models.CharField(max_length=100)
     country = CountryField(multiple=False)
     zip = models.CharField(max_length=5)
+    address_type = models.CharField(max_length=1, choices=ADDRESS_CHOICES)
+    default = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
+
+    class Meta:
+        verbose_name_plural = 'Addresses'
 
 
 class Payment(models.Model):
